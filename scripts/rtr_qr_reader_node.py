2bfb9510d2bb6e945c3e009fa21bcdcf1ef06505
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2021-10-06 03:00:09 +0900

    Display position

--- scripts/rtr_qr_reader_node.py
+++ scripts/rtr_qr_reader_node.py
@@ -8,18 +8,26 @@ import cv2
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from jsk_rviz_plugins.msg import OverlayText
+from rtr_quadcopter_controller.srv import QRPosition
 
 bridge = CvBridge()
+
 
 class Node:
     def __init__(self):
         rospy.init_node("rtr_qr_reader_node")
         topic = rospy.get_param("/qr_reader/topic_name", "/usb_cam/image_raw")
 
+        rospy.wait_for_service("/quadcopter/qr_position")
+        self.qr_position = rospy.ServiceProxy(
+                "/quadcopter/qr_position", QRPosition)
+
         self.img_sub = rospy.Subscriber(
             topic, Image, self.img_callback, queue_size=100)
 
-        self.output_pub = rospy.Publisher("qr_output", OverlayText, queue_size=10)
+        self.output_pub = rospy.Publisher(
+            "qr_output", OverlayText, queue_size=10)
+        self.res = None
 
     def img_callback(self, img_msg):
         img = bridge.imgmsg_to_cv2(img_msg, "bgr8")
@@ -30,9 +38,25 @@ class Node:
         print(data[0][1])
         print(data[0][2])
         print(data[0][3])
+        x = 0
+        y = 0
+        for p in data[0][3]:
+            x += p.x
+            y += p.y
+        x /= 4
+        y /= 4
+        x = int(x)
+        y = int(y)
 
+        print(x, y)
+        try:
+            self.res = self.qr_position(x, y)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s" % e)
+
+        print(self.res)
         text = OverlayText()
-        text.text = data[0][0].decode('utf-8', 'ignore')
+        text.text = data[0][0].decode('utf-8', 'ignore') + ': {:.3g}'.format(self.res.qr_global_x) + ", " + '{:.3g}'.format(self.res.qr_global_y) + ", " + '{:.3g}'.format(self.res.qr_global_z)
         text.width = 500
         text.height = 500
         text.text_size = 12
@@ -45,6 +69,7 @@ class Node:
         text.fg_color.b = 1
         text.fg_color.a = 1
         self.output_pub.publish(text)
+
 
 if __name__ == '__main__':
     node = Node()
